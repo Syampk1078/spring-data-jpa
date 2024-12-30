c1a779bd18fafe40ad20131d10bfb185d6243665
KANKANALA SYAM PRASAD <119304851+Syampk1078@users.noreply.github.com>
2024-12-31 01:40:39 +0530

    Cofigure the repo and entity

--- src/main/java/com/example/demo/SpringDataJpaApplication.java
+++ src/main/java/com/example/demo/SpringDataJpaApplication.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.entity.Player;
+import com.example.demo.repository.PlayerRepository;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJpaApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringDataJpaApplication.class, args);
+		
+		Player player = new Player();
+		player.setPlayerId(101);
+		player.setPlayerName("Syam");
+		player.setPlayerAge(24);
+		player.setLocation("Vizianagaram");
+		
+		PlayerRepository repo = context.getBean(PlayerRepository.class);
+		repo.save(player); //upsert method
+		
 	}
 
 }
